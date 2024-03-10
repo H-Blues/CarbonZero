37c6680c802b10c6d2ac430932807e12772b367c
H-Blues <hblues419@gmail.com>
2024-03-10 03:02:42 +0000

    config: the wallet connection  configuration

---
+++ frontend/src/context/index.js
@@ -0,0 +1,33 @@
+import { config, projectId } from '@/config'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { createWeb3Modal } from '@web3modal/wagmi/react'
+import React, { ReactNode } from 'react'
+import { State, WagmiProvider } from 'wagmi'
+
+const queryClient = new QueryClient()
+
+if (!projectId) {
+  throw new Error('Project ID is not defined')
+}
+
+createWeb3Modal({
+  wagmiConfig: config,
+  projectId,
+  themeMode: 'light',
+  themeVariables: {
+    '--w3m-accent': 'green',
+  }
+})
+
+function ContextProvider({
+  children,
+  initialState
+}) {
+  return (
+    <WagmiProvider config={config} initialState={initialState}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
+  )
+}
+
+export default ContextProvider
